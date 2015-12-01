52d8ee83117f4c8df7c3114994ecea1f5c809a4a
vadix <vadixidav@gmail.com>
2015-12-01 11:58:09 -0500

    bumped version; reworked and added to particle API to include more-useful particle physics functions that avoid divide by zero errors

--- src/particle/mod.rs
+++ src/particle/mod.rs
@@ -34,9 +34,11 @@ pub trait PhysicsParticle<V, D>: Particle<V, D> + Quanta<D>
         let delta = rhs.position() - lhs.position();
         //Find the distance of the delta vector
         let distance = delta.displacement();
-        //Dividing the delta vector by the distance cubed computes attractive force as per the inverse square law
-        //Multiply by the two quanta of the objects to compute the final force
-        let force = delta / distance.powi(3) * lhs.quanta() * rhs.quanta();
+        //Dividing the delta vector by the distance cubed computes attractive force as per the inverse square law.
+        //The extra degree normalizes the direction vector to a unit vector.
+        let force = delta / distance.powi(3) *
+        //Multiply by the two quanta of the objects to compute the final force.
+        lhs.quanta() * rhs.quanta();
 
         //Accelerate lhs in the direction of force and divide it by the inertia
         let acceleration = force / lhs.inertia();
@@ -44,6 +46,35 @@ pub trait PhysicsParticle<V, D>: Particle<V, D> + Quanta<D>
         //Apply the inverse force to the rhs similarly
         let acceleration = -force / rhs.inertia();
         rhs.accelerate(&acceleration);
+    }
+
+    /*
+    If gravitating at a distance greater than the radius, then gravitational interaction is applied as if the
+    particles are point particles. If the distance is less than the radius, then the interaction happens as if the
+    gravitational quanta (mass) is evenly distributed and gravitational flux is used instead, which causes the
+    interaction to become proportional to the radius, meaning that as the radius approaches zero, so does the force.
+
+    The radius is passed squared for internal efficiency reasons.
+    */
+    fn gravitate_radius_squared(lhs: &mut Self, rhs: &mut Self, radius_squared: D) {
+        let delta = rhs.position() - lhs.position();
+        let distance_squared = delta.displacement_squared();
+        //Force is the only thing that changes from normal gravitation
+        let force = delta * lhs.quanta() * rhs.quanta() / if distance_squared > radius_squared {
+            distance_squared.sqrt().powi(3)
+        } else {
+            radius_squared
+        };
+        let acceleration = force / lhs.inertia();
+        lhs.accelerate(&acceleration);
+        let acceleration = -force / rhs.inertia();
+        rhs.accelerate(&acceleration);
+    }
+
+    //This function exists so that if an optimization is possible later, it can be specially implemented.
+    //Presently it just acts as a frontend to its squared counterpart. Prefer squared version if possible.
+    fn gravitate_radius(lhs: &mut Self, rhs: &mut Self, radius: D) {
+        Self::gravitate_radius_squared(lhs, rhs, radius);
     }
 
     //Apply proper attraction to a single physics particle towards a location and with a magnitude
@@ -55,8 +86,40 @@ pub trait PhysicsParticle<V, D>: Particle<V, D> + Quanta<D>
         self.accelerate(&acceleration);
     }
 
+    //Works the same as gravitate_radius_squared and gravitate_to
+    fn gravitate_radius_squared_to(&mut self, location: V, magnitude: D, radius_squared: D) {
+        //Create delta vector from the particle to the center of attraction
+        let delta = location - self.position();
+        let distance_squared = delta.displacement_squared();
+        let acceleration = delta * magnitude * self.quanta() / (
+                self.inertia() *
+                if distance_squared > radius_squared {
+                    distance_squared.sqrt().powi(3)
+                } else {
+                    radius_squared
+                }
+            );
+        self.accelerate(&acceleration);
+    }
+
+    //This function exists so that if an optimization is possible later, it can be specially implemented.
+    //Presently it just acts as a frontend to its squared counterpart. Prefer squared version if possible.
+    fn gravitate_radius_to(&mut self, location: V, magnitude: D, radius: D) {
+        self.gravitate_radius_squared_to(location, magnitude, radius);
+    }
+
+    //Apply spring forces between two particles
+    fn hooke(lhs: &mut Self, rhs: &mut Self) {
+        let delta = rhs.position() - lhs.position();
+        let force = delta.normalized() * delta.displacement() * lhs.quanta() * rhs.quanta();
+        let acceleration = force / lhs.inertia();
+        lhs.accelerate(&acceleration);
+        let acceleration = -force / rhs.inertia();
+        rhs.accelerate(&acceleration);
+    }
+
     //Apply spring forces between two particles with specified equilibrium distance
-    fn hooke(lhs: &mut Self, rhs: &mut Self, equilibrium: D) {
+    fn hooke_equilibrium(lhs: &mut Self, rhs: &mut Self, equilibrium: D) {
         let delta = rhs.position() - lhs.position();
         let force = delta.normalized() *
             //We scale such that if displacement is greater than equilibrium, the particles attract proportionally
@@ -69,8 +132,16 @@ pub trait PhysicsParticle<V, D>: Particle<V, D> + Quanta<D>
         rhs.accelerate(&acceleration);
     }
 
+    //Apply spring forces between one particle and a position with a magnitude
+    fn hooke_to(&mut self, location: V, magnitude: D) {
+        let delta = location - self.position();
+        let acceleration = delta.normalized() * delta.displacement() *
+            magnitude * self.quanta() / self.inertia();
+        self.accelerate(&acceleration);
+    }
+
     //Apply spring forces between one particle and a position with an equilibrium and magnitude
-    fn hooke_to(&mut self, location: V, magnitude: D, equilibrium: D) {
+    fn hooke_equilibrium_to(&mut self, location: V, magnitude: D, equilibrium: D) {
         let delta = location - self.position();
         let acceleration = delta.normalized() * (delta.displacement() - equilibrium) *
             magnitude * self.quanta() / self.inertia();
@@ -91,6 +162,26 @@ pub trait PhysicsParticle<V, D>: Particle<V, D> + Quanta<D>
         rhs.accelerate(&acceleration);
     }
 
+    //Apply lorentz forces between two PhysicsParticle objects with a radius_squared
+    fn lorentz_radius_squared(lhs: &mut Self, rhs: &mut Self, radius_squared: D)
+        where V: CrossVector<D>
+    {
+        let delta = rhs.position() - lhs.position();
+        let distance_squared = delta.displacement_squared();
+        let force = rhs.velocity().cross(&lhs.velocity().cross(&delta)) * lhs.quanta() * rhs.quanta() /
+            if distance_squared > radius_squared {
+                distance_squared.sqrt().powi(3)
+            } else {
+                radius_squared
+            };
+
+        let acceleration = -force / lhs.inertia();
+        lhs.accelerate(&acceleration);
+        let acceleration = force / rhs.inertia();
+        rhs.accelerate(&acceleration);
+    }
+
+    //Apply lorentz force to a particle in a field given by a vector with the magnitude and direction of the field
     fn lorentz_field(&mut self, field: &V)
         where V: CrossVector<D>
     {
