4a796401f3082608a3230e58e894ef4d47dd90f2
Geordon Worley <vadixidav@gmail.com>
2016-01-10 18:56:14 -0500

    removed all trees from library

--- src/barneshut/cartesian3.rs
+++
@@ -1,9 +0,0 @@
-extern crate num;
-use self::num::Float;
-use super::super::vector::Vector;
-use super::super::particle::{Particle, Quanta};
-use super::BarnesHut;
-
-struct Tree {
-
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 pub mod vector;
 pub mod particle;
-pub mod tree;
-//pub mod barneshut;
 pub use vector::*;
 pub use particle::*;
-//pub use tree::*;
-//pub use barneshut::*;
