b2f48c7106a63f5ce8e469ceecdd83c18369820e
Geordon Worley <vadixidav@gmail.com>
2016-02-14 21:51:59 -0500

    add some notes to the crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Please do not "use zoom::*" or the zoom::Box type will override std::boxed::Box!
+//!
+//! This crate creates traits for driving particle physics functions and doing simple
+//! vector geometry. It also contains various particle interactions in the particle module, which
+//! is publically used in zoom.
+
 pub mod vector;
 pub mod particle;
 pub use vector::*;
